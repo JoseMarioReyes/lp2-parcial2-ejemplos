87406cbf4a5ca3506513773967a59f24d48875ec
José Mario Reyes <josmar131@gmail.com>
2022-10-24 19:59:37 -0600

    Add project files.

---
+++ constructor-setter-getter/Persona.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace constructor_setter_getter
+{
+    class Persona
+    {
+        private string PrimerNombre;
+        private string SegundoNombre;
+        private string PrimerApellido;
+        private string SegundoApellido;
+
+        public Persona()
+        { }
+
+        public Persona(string PrimerNombre, string PrimerApellido)
+        {
+            this.PrimerNombre = PrimerNombre;
+            this.PrimerApellido = PrimerApellido;
+        }
+
+        public Persona(string PrimerNombre,
+            string PrimerApellido,
+            string SegundoNombre,
+            string SegundoApellido)
+        {
+            this.PrimerNombre = PrimerNombre;
+            this.PrimerApellido = PrimerApellido;
+            this.SegundoNombre = SegundoNombre;
+            this.SegundoApellido = SegundoApellido;
+        }
+
+        public void setPrimerNombre(string PrimerNombre)
+        {
+            this.PrimerNombre = PrimerNombre;
+        }
+
+        public string getPrimerNombre()
+        {
+            return this.PrimerNombre;
+        }
+
+        public string getNombreCompleto()
+        {
+            return $"{PrimerNombre} {SegundoNombre} {PrimerApellido} {SegundoApellido}";
+        }
+    }
+}
---
+++ constructor-setter-getter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace constructor_setter_getter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona unaPersona = new Persona("Jose", "Aguilar");
+            Console.WriteLine(unaPersona.getNombreCompleto());
+
+            Console.ReadLine();
+        }
+    }
+}
