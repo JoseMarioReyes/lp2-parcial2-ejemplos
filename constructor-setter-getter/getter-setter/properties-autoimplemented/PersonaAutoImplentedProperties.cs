c107004e1fdb74da97a648cb2bb0606e5f1506a6
José Mario Reyes <josmar131@gmail.com>
2022-10-31 19:17:52 -0600

    Agregar el ejemplo de propiedades autoimplementadas.

---
+++ constructor-setter-getter/getter-setter/properties-autoimplemented/PersonaAutoImplentedProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace constructor_setter_getter.getter_setter.properties_autoimplemented
+{
+    class PersonaAutoImplentedProperties
+    {
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+    }
+}
